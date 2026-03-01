42a158f16e3d567f33e6b37b1f1376b87ffef33d
ARYAN AFRIDI <shahid2005afridi@gmail.com>
2026-03-01 23:45:28 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get install php -y 
-sudo apt-get install wget -y
-sudo apt-get install unzip -y 
-sudo apt-get install curl -y 
-sudo apt-get install figlet toilet -y 
-chmod +x DGTLMAN.sh
-bash DGTLMAN.sh
